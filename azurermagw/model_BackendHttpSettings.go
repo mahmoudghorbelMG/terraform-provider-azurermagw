d06d1899a97f4489b767b4aee6011d3956d5e012
Mahmoud Ghorbel <mahmoud.ghorbel.p@citeo.com>
2022-08-08 08:36:11 +0200

    probe name true/false

--- azurermagw/model_BackendHttpSettings.go
+++ azurermagw/model_BackendHttpSettings.go
@@ -115,8 +115,7 @@ func createBackendHTTPSettings(backend_plan Backend_http_settings,AZURE_SUBSCRIP
 		}
 	}else{
 		//backend_json.Properties.Probe = 
-	}
-		
+	}		
 	fmt.Printf("\nHHHHHHHHHHHHHH  backend_json.Properties.Probe =\n %+v ",backend_json.Properties.Probe)
 	
 	// add the backend to the agw and update the agw
@@ -155,10 +154,10 @@ func generateBackendHTTPSettingsState(gw ApplicationGateway, BackendHTTPSettings
 	}else{
 		backend_state.Affinity_cookie_name = types.String{Null: true}
 	}
-	if (&backend_json.Properties.PickHostNameFromBackendAddress != nil){
-		backend_state.Pick_host_name_from_backend_address = types.Bool {Value: bool(backend_json.Properties.PickHostNameFromBackendAddress)}
+	if backend_json.Properties.PickHostNameFromBackendAddress {
+		backend_state.Pick_host_name_from_backend_address = types.Bool {Value: true}
 	}else{
-		//backend_state.Pick_host_name_from_backend_address = types.Bool {Value: false}
+		backend_state.Pick_host_name_from_backend_address = types.Bool {Value: false}
 	}
 
 	fmt.Printf("\n--------------------- BackendHTTPSettings state createBackendHTTPSettings() =\n %+v ",backend_state)
